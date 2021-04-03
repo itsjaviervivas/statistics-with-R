bbaa07ae676b0eef9057d5923c129dd7c2ac8d3b
Javier Vivas <javierfvivasc@protonmail.com>
2021-04-03 15:07:54 -0500

    Create tallernash2.R
    Hola bebé, aquí los puntos del Taller Nash del 3 al 7

---
+++ tallernash2.R
@@ -0,0 +1,41 @@
+#Intervalo de Confianza
+#Punto 3-----
+p <- 0.25
+n <- 400
+std_error <- sqrt(p*(1-p)/n)
+alpha <- 0.05
+z <- qnorm(1-alpha/2)
+ll <- p - z*std_error
+ul <- p + z*std_error
+cat((1-alpha)*100,"% CI: (", round(ll, 3), ", ", round(ul, 3), ")")
+
+#Punto 4 ----
+d <- 60-66 # y1 - y2
+alpha <- 1 - 0.99 # 1 - confidence coefficient
+sigma1 <- 19  # standar deviations
+sigma2 <- 16
+n1 <- 20      # sample size
+n2 <- 25
+z <- qnorm(1-alpha/2)
+Margin_Error <- z*sqrt((sigma1^2/n1)+(sigma2^2/n2)) # Formula
+ll <- d - Margin_Error # Lower limit
+ul <- d + Margin_Error # upper limit
+cat((1-alpha)*100,"% CI: (", round(ll, 3), ", ", round(ul, 3), ")")
+#Punto 7------
+n <- 600
+sd <- 175           
+y <- 649 
+alpha <- 0.05
+
+ll <- y-qnorm(1-alpha/2) * sd /sqrt(n)
+ul <- y+qnorm(1-alpha/2) * sd /sqrt(n)
+cat((1-alpha)*100,"% CI: (", round(ll, 3), ", ", round(ul, 3), ")")
+
+
+#Punto 8----
+#Creamos el vector de los datos
+
+x<-c(7.0, 3.2, 1.4, 5.4, 8.5, 2.5, 2.5, 1.9, 5.4, 1.6, 1.0, 2.1, 8.5, 4.3, 6.2, 
+     1.5, 1.2, 2.7, 3.8, 2.0, 1.2, 2.6, 4.0, 2.6,0.6)
+#Calculamos la media
+mean(x)
